de3c32ceb3c60de822f35987179d6c69b361d351
Dan Samama <dan.samama@gmail.com>
2016-07-07 20:39:21 +0300

    Adding some comment to understand better Game finished Landing page finished

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,1 +1,18 @@
+var tutorial = $('<div/>');
+tutorial.addClass("mainTutorialDiv");
+$('.main-stripe').append(tutorial);
 
+
+var listOfInstructions = $('<ol/>');
+$('.mainTutorialDiv').append(tutorial);
+
+
+var instruction1 = $('<li/>');
+$(listOfInstructions).append(instruction1);
+instruction1.text("Select the tools that you want to use according to the following rules:"<br>
+    "The pickaxe can only remove : stones" <br>
+    "The axe can only remove : trunk and leaves");
+
+var instruction2 = $('<li/>');
+$(listOfInstructions).append(instruction2);
+instruction2.text = "Play the game";
