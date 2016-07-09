8cad17b611ce8ec8efdb5a50b65e696ae922b398
Dan Samama <dan.samama@gmail.com>
2016-07-09 19:23:44 +0300

    Adding some comment to understand better Game finished Landing page finished Landing page improvement. Still few things to fix

--- js/landing-page.js
+++ js/landing-page.js
@@ -4,15 +4,25 @@ $('.main-stripe').append(tutorial);
 
 
 var listOfInstructions = $('<ol/>');
-$('.mainTutorialDiv').append(tutorial);
+listOfInstructions.addClass("liste");
+$('.mainTutorialDiv').append(listOfInstructions);
 
 
 var instruction1 = $('<li/>');
+instruction1.addClass('instruction1');
 $(listOfInstructions).append(instruction1);
-instruction1.text("Select the tools that you want to use according to the following rules:"<br>
-    "The pickaxe can only remove : stones" <br>
-    "The axe can only remove : trunk and leaves");
+instruction1.text("Select the tools that you want to use according to the following rules:  The pickaxe can only remove : stone, The axe can only remove : trunk and leaves, The shovel can only remove : grass and dirt");
 
 var instruction2 = $('<li/>');
 $(listOfInstructions).append(instruction2);
-instruction2.text = "Play the game";
+instruction2.text("Play the game");
+
+
+$('#instructions').on('click',function () { // TODO Try with the hide() method. Hide the tutorial div and on click make it appears
+    // if($('.mainTutorialDiv').display === none) {
+        $('.mainTutorialDiv').css('display', 'inline-block');
+    // }
+    // else{
+    //     $('.mainTutorialDiv').css('display', 'none');
+    // }
+});
